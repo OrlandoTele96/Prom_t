161639a45a8386afb4f92dc79769768d7dcaedab
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-09-01 21:48:00 -0500

    changes

---
+++ src/main/java/com/example/prom_app/RegistrateActivity.java
@@ -0,0 +1,14 @@
+package com.example.prom_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrate);
+    }
+}
--- src/main/java/com/example/prom_app/WelcomeActivity.java
+++ src/main/java/com/example/prom_app/WelcomeActivity.java
@@ -2,9 +2,9 @@ package com.example.prom_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.TextureView;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -14,10 +14,10 @@ import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    TextView tvNameapp,tvWelcomeMessage;
+    TextView tvNameapp,tvWelcomeMessage,tvRegistration;
     ImageView ivBrand,ivBackGround;
-    ImageButton ibtnLogin;
-    Animation fromloginbtn,fromappname;
+    ImageButton ibtnLogin,ibtnRegistration;
+    Animation frominit;
 
     User user= new User("","","","","");;
 
@@ -28,13 +28,16 @@ public class WelcomeActivity extends AppCompatActivity {
 
         tvNameapp = findViewById(R.id.tvNameapp);
         tvWelcomeMessage = findViewById(R.id.tvWelcomeMessage);
+        tvRegistration = findViewById(R.id.tvRegistration);
         ivBackGround = findViewById(R.id.ivBackGround);
         ivBrand = findViewById(R.id.ivbrand);
         ibtnLogin = findViewById(R.id.ibtnLogin);
+        ibtnRegistration = findViewById(R.id.ibtnRegistration);
 
         tvNameapp.setText(getResources().getString(R.string.Prom_app));
         tvNameapp.setTextColor(getResources().getColor(R.color.White));
         tvWelcomeMessage.setText(getResources().getString(R.string.wel));
+        tvRegistration.setText(getResources().getString(R.string.register));
 
         // Wait for database response
         /*Thread timer = new Thread(){
@@ -49,28 +52,46 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         };*/
         //timer.start();
+
         if (Validation(user))
         {
-            Log.d("WELCOME","User ID is validated");
+            Log.d(getResources().getString(R.string.welcome),getResources().getString(R.string.wel_validated));
             //Entry to main activity
         }
         else
         {
             // Splash
-            Log.d("WELCOME", "User is not validated");
+            Log.d(getResources().getString(R.string.welcome),getResources().getString(R.string.wel_nvalidated));
             ivBackGround.animate().translationY(-900).setDuration(800).setStartDelay(1000);
             ivBrand.animate().translationY(-170).setDuration(800).setStartDelay(1100);
-            fromloginbtn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fromloginbtn);
-            //fromappname = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fromappname);
-            ibtnLogin.setAnimation(fromloginbtn);
-            ibtnLogin.startAnimation(fromloginbtn);
-            //tvNameapp.setAnimation(fromappname);
-            //tvNameapp.startAnimation(fromappname);
-            tvNameapp.animate().translationY(-150).setDuration(800).setStartDelay(1100);
+            frominit = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.init);
+            ibtnLogin.setAnimation(frominit);
+            ibtnLogin.startAnimation(frominit);
+            tvNameapp.animate().alpha(0).translationY(-150).setDuration(800).setStartDelay(1100);
             tvNameapp.setTextColor(getResources().getColor(R.color.Black));
             tvWelcomeMessage.animate().alpha(0).setDuration(200).setStartDelay(1100);
-            tvWelcomeMessage.setTextColor(getResources().getColor(R.color.Black));
+            tvRegistration.setAnimation(frominit);
+            ibtnLogin.startAnimation(frominit);
+            ibtnRegistration.setAnimation(frominit);
         }
+
+
+        ibtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Fields for log in
+            }
+        });
+
+        ibtnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Activity for registration
+                Intent intent = new Intent(WelcomeActivity.this,RegistrateActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     boolean Validation(User u)
