d44681b0c174b1590f7d522f9e48fb367c92d116
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-09-04 19:06:53 -0500

    LOG  activity

--- src/main/java/com/example/prom_app/WelcomeActivity.java
+++ src/main/java/com/example/prom_app/WelcomeActivity.java
@@ -8,16 +8,19 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    TextView tvNameapp,tvWelcomeMessage,tvRegistration;
+    TextView tvWelcomeMessage;
     ImageView ivBrand,ivBackGround;
     ImageButton ibtnLogin,ibtnRegistration;
-    Animation frominit;
+    EditText etEmail,etPswd;
+
+    Animation frombg,frombrand,fromwhiteletters,frominit,frombg_trans;
 
     User user= new User("","","","","");;
 
@@ -26,18 +29,22 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
 
-        tvNameapp = findViewById(R.id.tvNameapp);
+        //tvNameapp = findViewById(R.id.tvNameapp);
         tvWelcomeMessage = findViewById(R.id.tvWelcomeMessage);
-        tvRegistration = findViewById(R.id.tvRegistration);
+        //tvRegistration = findViewById(R.id.tvRegistration);
         ivBackGround = findViewById(R.id.ivBackGround);
         ivBrand = findViewById(R.id.ivbrand);
         ibtnLogin = findViewById(R.id.ibtnLogin);
         ibtnRegistration = findViewById(R.id.ibtnRegistration);
+        etEmail = findViewById(R.id.etEmail);
+        etPswd = findViewById(R.id.etPswd);
 
-        tvNameapp.setText(getResources().getString(R.string.Prom_app));
-        tvNameapp.setTextColor(getResources().getColor(R.color.White));
+        //tvNameapp.setText(getResources().getString(R.string.Prom_app));
+        //tvNameapp.setTextColor(getResources().getColor(R.color.White));
         tvWelcomeMessage.setText(getResources().getString(R.string.wel));
-        tvRegistration.setText(getResources().getString(R.string.register));
+        etEmail.setHint(getResources().getString(R.string.txtemail));
+        etPswd.setHint(getResources().getString(R.string.txtpswd));
+        //tvRegistration.setText(getResources().getString(R.string.register));
 
         // Wait for database response
         /*Thread timer = new Thread(){
@@ -62,15 +69,25 @@ public class WelcomeActivity extends AppCompatActivity {
         {
             // Splash
             Log.d(getResources().getString(R.string.welcome),getResources().getString(R.string.wel_nvalidated));
-            ivBackGround.animate().translationY(-900).setDuration(800).setStartDelay(1000);
-            ivBrand.animate().translationY(-170).setDuration(800).setStartDelay(1100);
+            frombg = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.frombg);
+            ivBackGround.setAnimation(frombg);
+            ivBackGround.startAnimation(frombg);
+            frombrand = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.frombrand);
+            ivBrand.setAnimation(frombrand);
+            ivBrand.startAnimation(frombrand);
+            fromwhiteletters=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fromwhiteletters);
+            //tvNameapp.setAnimation(fromwhiteletters);
+            //tvNameapp.startAnimation(fromwhiteletters);
+            tvWelcomeMessage.setAnimation(fromwhiteletters);
+            tvWelcomeMessage.startAnimation(fromwhiteletters);
             frominit = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.init);
             ibtnLogin.setAnimation(frominit);
             ibtnLogin.startAnimation(frominit);
-            tvNameapp.animate().alpha(0).translationY(-150).setDuration(800).setStartDelay(1100);
-            tvNameapp.setTextColor(getResources().getColor(R.color.Black));
-            tvWelcomeMessage.animate().alpha(0).setDuration(200).setStartDelay(1100);
-            tvRegistration.setAnimation(frominit);
+            etEmail.setAnimation(frominit);
+            etEmail.startAnimation(frominit);
+            etPswd.setAnimation(frominit);
+            etPswd.startAnimation(frominit);
+            //tvRegistration.setAnimation(frominit);
             ibtnLogin.startAnimation(frominit);
             ibtnRegistration.setAnimation(frominit);
         }
